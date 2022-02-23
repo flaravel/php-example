ed1ea819470c1253305c751c191164bb1dbdba33
xijun <flaravel@gmail.com>
2022-02-24 02:12:05 +0800

    add redis lock

--- redis-lock/RedisCache.php
+++ redis-lock/RedisCache.php
@@ -2,11 +2,15 @@
 
 class RedisCache
 {
+    protected static ?Redis $instance = null;
 
     public static function getInstance(): Redis
     {
-        $redis = new Redis();
-        $redis->connect('127.0.0.1', '6379');
-        return $redis;
+        if (!is_null(self::$instance)) {
+            return self::$instance;
+        }
+        self::$instance = new Redis();
+        self::$instance->connect('127.0.0.1', '6379');
+        return self::$instance;
     }
 }
